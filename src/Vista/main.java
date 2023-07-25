fc00ef1488b7a23fb44b486f83b84a3375368cbc
Jhonatan Mantilla <jhonatanmm.1995@gmail.com>
2023-07-24 19:20:48 -0500

    implementando nuevas funciones y algunas mejoras de codigo

--- src/Vista/main.java
+++ src/Vista/main.java
@@ -6,28 +6,24 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String[] args) {
-
-        Runnable mRun;
-        mRun = new Runnable() {
-            @Override
-            public void run() {
-                // JFrame que funge como splash
-                SplashScreen mSplash = new SplashScreen();
-                mSplash.setVisible(true);
-
-                try {
-                    Thread.sleep(2500); // 5000 milisegundos equivale a 5 segundos.
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                }
-
-                mSplash.dispose();
-
-                // JFrame que funge como pantalla principal
-                new FrmConvertor().setVisible(true);
+        Runnable mRun = () -> {
+            
+            // JFrame que funge como splash
+            SplashScreen mSplash = new SplashScreen();
+            mSplash.setVisible(true);
+            
+            try {
+                Thread.sleep(3000); // 5000 milisegundos equivale a 5 segundos.
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             }
+            
+            mSplash.dispose();
+            
+            // JFrame que funge como pantalla principal
+            new FrmConvertor().setVisible(true);
         };
         Thread mHiloSplash = new Thread(mRun);
-        mHiloSplash.start();
-    }
+        mHiloSplash.start();  
+    } 
 }
